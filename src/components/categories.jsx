56ba1d21346a03758b80457ca475f54cb2e09906
Lautaro Vazquez <lautivp16@gmail.com>
2024-03-12 20:12:44 -0300

    categorias

--- src/components/categories.jsx
+++ src/components/categories.jsx
@@ -1,45 +1,40 @@
-import { Link } from "react-router-dom"
-import { Cart } from "./Cart"
+import { Link } from "react-router-dom";
+import { Cart } from "./Cart";
+
 export const Categories = () => {
     return (
         <>
             <li className="text-white">
                 <Link to={'/'}>
                     <button className="bg-teal-500 text-white px-4 py-2 rounded flex items-center">
-                        <home />
                         <span>Inicio</span>
                     </button>
                 </Link>
-
             </li>
             <li className="text-white">
                 <Link to={'/category/Urban'}>
                     <button className="bg-teal-500 text-white px-4 py-2 rounded flex items-center">
-                        <urban/>
                         <span>Urbanas</span>
                     </button>
                 </Link>
-                </li>
-                <li className="text-white">
+            </li>
+            <li className="text-white">
                 <Link to={'/category/Skate'}>
                     <button className="bg-teal-500 text-white px-4 py-2 rounded flex items-center">
-                        <skate/>
                         <span>Skate</span>
                     </button>
                 </Link>
-                </li>
-                <li className="text-white">
+            </li>
+            <li className="text-white">
                 <Link to={'/category/Basketball'}>
                     <button className="bg-teal-500 text-white px-4 py-2 rounded flex items-center">
-                        <basketball/>
                         <span>Basquet</span>
                     </button>
                 </Link>
-                </li>
-                <li className="text-white">
-                <Link to={'/category/Fotball'}>
+            </li>
+            <li className="text-white">
+                <Link to={'/category/Football'}>
                     <button className="bg-teal-500 text-white px-4 py-2 rounded flex items-center">
-                        <football/>
                         <span>Futbol</span>
                     </button>
                 </Link>
@@ -47,12 +42,10 @@ export const Categories = () => {
             <li className="text-white">
                 <Link to={'/category/Formal'}>
                     <button className="bg-teal-500 text-white px-4 py-2 rounded flex items-center">
-                        <formal/>
                         <span>Formales</span>
                     </button>
                 </Link>
             </li>
-           
         </>
-    )
-}
+    );
+};
