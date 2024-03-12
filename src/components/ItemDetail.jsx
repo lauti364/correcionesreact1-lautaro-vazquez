a13ed2001c4fbe1fb3788c51beb94cb59bb3f765
Lautaro Vazquez <lautivp16@gmail.com>
2024-03-12 20:49:16 -0300

    diseño

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 import { useCounter } from "../hooks/useCounter"
 import { useCarritoContext } from "../context/CartContext"
+import './item.css'
 export const ItemDetail = ({ item }) => {
     const { addItem } = useCarritoContext()
     const { count, increment, decrement, reset } = useCounter(1, item.stock, 1)
@@ -19,7 +20,7 @@ export const ItemDetail = ({ item }) => {
         })
     }
     return (
-
+<div className="caja">
         <div className="bg-white p-8 rounded-lg max-w-md">
             <Link to={'/'}>
                 <button className="absolute top-4 right-4 text-gray-700 hover:text-gray-900">Cerrar</button>
@@ -28,7 +29,7 @@ export const ItemDetail = ({ item }) => {
             <img className="w-full h-64 object-cover mb-6" src={`${item.img}`} alt={`Imagen de ${item.title}`} />
             <div className="text-center">
                 <h2 className="text-2x1 font-bold mb-2">{item.title}</h2>
-                <p className="text-gray-700 mb-2 font-semibold">Tamaño maseta: {item.size}</p>
+                <p className="text-gray-700 mb-2 font-semibold">Talle: {item.size}</p>
                 <p className="text-gray-700 mb-2 font-semibold">Stock: {item.stock}</p>
                 <p className="text-gray-700 mb-2 font-semibold">Precio: ${item.price}</p>
                 <div className="container mx-auto mt-8">
@@ -50,7 +51,7 @@ export const ItemDetail = ({ item }) => {
                 </div>
             </div>
         </div>
-
+        </div>
 
     )
 }
