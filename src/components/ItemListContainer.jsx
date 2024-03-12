110d5b239a2211652ed3b3dd87390b8ce5bd1eb9
Lautaro Vazquez <lautivp16@gmail.com>
2024-03-12 16:34:07 -0300

    Item

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 import { useParams } from 'react-router-dom'
-import { ItemList } from "./ItemList"
+import { ItemList } from "./ItemList.jsx"
 import { getProducts } from "../firebase/firebase.js"
 export const ItemListContainer = () => {
     const [products, setProducts] = useState([])
